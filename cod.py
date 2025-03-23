4db009fd4b6b30aa3421ef9abba42e2e0e887b7f
v7M0K <141871763+v7m0k@users.noreply.github.com>
2025-03-23 15:27:50 -0300

    cod.py

--- cod.py
+++ cod.py
@@ -6,10 +6,16 @@ def download_video(url, format):
     output_path = "downloads"
     os.makedirs(output_path, exist_ok=True)
     
+    # evitar o bloqueio do yt
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+    
     options = {
         'format': 'bestaudio/best' if format == 'mp3' else 'bestvideo+bestaudio',
         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
-        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}] if format == 'mp3' else []
+        'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}] if format == 'mp3' else [],
+        'http_headers': headers,  # http
     }
     
     with yt_dlp.YoutubeDL(options) as ydl:
@@ -24,12 +30,12 @@ st.set_page_config(page_title="YouTube Downloader", page_icon="🎵", layout="ce
 st.markdown("""
     <style>
     .stApp {
-        background-color: black;
-        color: white;
+        background-color: #1C1C1C;
+        color: whitesmoke;
     }
     .stButton>button {
-        background-color: red;
-        color: white;
+        background-color: #A52A2A;
+        color: whitesmoke;
         font-size: 18px;
     }
     </style>
